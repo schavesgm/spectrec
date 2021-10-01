13432926404738fdeef87ae98da4e42b0f58b885
Sergio Chaves Garcia-Mascaraque <sergiozteskate@gmail.com>
2021-10-01 16:40:44 +0100

    New version of the scripts

--- scripts/train_net.py
+++ scripts/train_net.py
@@ -1,20 +1,21 @@
 # Load some built-in modules
-import os, re
+import os, re, json, datetime
 
 # Load some third-party modules
 import torch
 import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
 
 # Load the basic classic
 from spectrec.factory import SpectralDataset
-from spectrec.network import UNet
 from spectrec.losses  import MSELoss
 
 # Load some utility functions and classes
-from spectrec.utils   import InputParser
+from spectrec.utils   import SpectrecInput
 from spectrec.utils   import train_network
 
 # Peaks and kernels to be registered in the factory
+from spectrec.network import UNet
 from spectrec.factory import GaussianPeak
 from spectrec.factory import DeltaPeak
 from spectrec.factory import NRQCDKernel
@@ -22,6 +23,7 @@ from spectrec.factory import NRQCDKernel
 # Load the factory functions to register some peaks and kernels
 from spectrec.utils import register_peak_class
 from spectrec.utils import register_kernel_class
+from spectrec.utils import register_network_class
 
 if __name__ == '__main__':
 
@@ -32,87 +34,49 @@ if __name__ == '__main__':
     # Register some kernels
     register_kernel_class('NRQCDKernel', NRQCDKernel)
 
+    # Register some networks
+    register_network_class('UNet', UNet)
+
     # Load a dictionary of input parameters
-    input_info = InputParser('./template.yml').parse_input()
+    spectrec_input = SpectrecInput('./template.yml')
 
-    # Get the information related to the dataset and network
-    dataset_info, network_info = input_info['dataset'], input_info['network']
-
-    # Get some important parameters from the dataset information
-    Nb, Ns = dataset_info['Nb'], dataset_info['Ns']
-    Nw, Nt = dataset_info['Nw'], dataset_info['Nt']
+    # Save the information into tensorboard
+    spectrec_input.write_to_tensorboard('./status/runs')
 
     # Device where the data will be stored
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # Generate a kernel
-    kernel = dataset_info['kernel'](Nt, Nw, dataset_info['w_range'])
+    # Get a dataset with the input parameters
+    dataset = spectrec_input.get_dataset(verbose=True)
 
-    # Generate a dataset using the input dictionary
-    dataset = SpectralDataset(
-        dataset_info['peak_types'], dataset_info['peak_limits'], kernel, 
-        dataset_info['max_np'], dataset_info['fixed_np'], dataset_info['peak_ids']
-    )
+    # Get the network to be used in the training
+    network = spectrec_input.get_network()
 
-    # Get the prefix and suffix to be added to the dataset
-    dataset_pre, dataset_suf = dataset_info['prefix'], dataset_info['suffix']
+    # Load the parameters of the network if possible
+    network.load_params('./status/network')
 
-    # Generate the dataset name to check if it already exists
-    name_id = dataset.name.replace('bNone_sNone', f'b{Nb}_s{Ns}')
-    name_id = f'{dataset_pre}_{name_id}' if dataset_pre else name_id
-    name_id = f'{name_id}_{dataset_suf}' if dataset_suf else name_id
-
-    # Generate the dataset path using the location and the name identifier
-    dataset_path = os.path.join(input_info['output'], 'dataset', name_id)
-
-    # Load the basis functions if the basis functions file path is passed
-    basis = torch.load(dataset_info['basis']) if dataset_info['basis'] else None
-
-    # Check if the given dataset already exists
-    if os.path.exists(dataset_path):
-        print(f' -- Dataset {name_id} exists')
-        if dataset_info['overwrite']:
-            if dataset_info['basis']:
-                print(f'    -- Overwriting dataset using basis: ' + dataset_info['basis'])
-            else:
-                print(f'    -- Overwriting dataset')
-            dataset.generate(Nb, Ns, basis=basis, use_GPU=dataset_info['use_GPU'])
-        else:
-            print(f'    -- Loading dataset')
-            dataset.load_dataset(Nb, Ns, dataset_pre, dataset_suf, os.path.join(input_info['output'], 'dataset'))
-    else:
-        print(f' -- Generating {name_id} dataset')
-        dataset.generate(Nb, Ns, basis=basis, use_GPU=dataset_info['use_GPU'])
-
-    # Generate a network to be used in the training
-    net = UNet(Nt, Ns)
-
-    # Generate the network name using id + Ns + Nb + Nt + Nw + max_np + fixed_np
-    net_name = re.sub(r'^b\d+_', '', dataset.name)
-    net_name = network_info['prefix'] + '_' + net_name if network_info['prefix'] else net_name
-
-    # Load the network parameters
-    net.load_params(net_name, path=os.path.join(input_info['output'], 'network'))
-
-    # Generate a loss function to be used in the training
+    # Generate the loss function used in the training
     loss = MSELoss()
 
-    # Monitor path
-    monitor_path = os.path.join(input_info['output'], 'monitor', net.net_id, net_name)
+    # Train the network using the parameters passed
+    train_network(network, dataset, loss, spectrec_input.parse_train_info(), device, spectrec_input.run_name)
 
-    # Train the network
-    train_network(
-        net, dataset, loss, network_info['epochs'], device, network_info['batch_size'], path=monitor_path
-    )
+    # Save the network parameters
+    network.save_params('./status/network')
 
-    # Save the network parameters to be used in the future
-    net.save_params(net_name, path=os.path.join(input_info['output'], 'network'))
+    # Get the prefix and suffix of the dataset to use it in the saving
+    dataset_info = spectrec_input.parse_dataset_info()
+    prefix, suffix = dataset_info['prefix'], dataset_info['suffix']
 
-    # Validate the dataset
-    dataset.test(
-        net, loss, network_info['val_prop'], device, network_info['batch_size'],
-        'val', prefix=dataset_pre, suffix=dataset_suf, path=monitor_path
-    )
+    # Save the dataset
+    dataset.save_dataset(prefix, suffix, './status/dataset')
 
-    # Save the current dataset
-    dataset.save_dataset(dataset_pre, dataset_suf, path=os.path.join(input_info['output'], 'dataset'))
+    # # Save the network parameters to be used in the future
+    # net.save_params(net_name, path=os.path.join(input_info['output'], 'network'))
+
+    # # Validate the dataset
+    # dataset.test(
+    #     net, loss, network_info['val_prop'], device, network_info['batch_size'],
+    #     'val', prefix=dataset_pre, suffix=dataset_suf, path=monitor_path
+    # )
+

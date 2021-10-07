2b0422db103f27e19f751cd3cfb16233dbe61b87
Sergio Chaves Garcia-Mascaraque <sergiozteskate@gmail.com>
2021-10-07 12:32:00 +0100

    Updated train script to allow local and SLURM training. Needs testing

--- scripts/train_net.py
+++ scripts/train_net.py
@@ -1,8 +1,11 @@
 # Load some built-in modules
 import argparse
+import random
+from pathlib import Path
 
 # Load some third-party modules
 import torch
+import submitit
 
 # Load some needed spectrec utilities
 from spectrec.utils   import SpectrecInput
@@ -13,22 +16,29 @@ from spectrec.training import initialise_dist_nodes
 from spectrec.training import initiliase_dist_gpu
 from spectrec.training import Trainer
 from spectrec.training import generate_validation_sets
+from spectrec.training import get_slurm_output_directory
 
-def train(gpu: int, config_file: str, node_info: dict):
+def train(gpu: int, args):
     """ Main train function to be spawned in the distributed architecture. """
 
     # Initialise the distributed gpu
-    rank = initiliase_dist_gpu(gpu, node_info)
+    initiliase_dist_gpu(gpu, args)
 
     # Generate a spectrec input loader
-    spectrec_input = SpectrecInput(config_file)
+    spectrec_input = SpectrecInput(args.config)
 
     # Register the external classes in the system
     spectrec_input.register_classes(verbose=True)
 
-    # Get the training info from the loader
-    train_info   = spectrec_input.parse_train_info()
-    dataset_info = spectrec_input.parse_dataset_info()
+    # Set some needed information from the input into args
+    for key, value in spectrec_input.parse_train_info().items():
+        setattr(args, key, value)
+
+    for key, value in spectrec_input.parse_dataset_info().items():
+        setattr(args, key, value)
+
+    for key, value in spectrec_input.parse_network_info().items():
+        setattr(args, key, value)
 
     # Save the information into tensorboard
     spectrec_input.write_to_tensorboard('./status/runs')
@@ -37,61 +47,113 @@ def train(gpu: int, config_file: str, node_info: dict):
     dataset = spectrec_input.get_dataset(verbose=True)
 
     # Save the dataset if possible
-    dataset.save_dataset(dataset_info['prefix'], dataset_info['suffix'])
+    dataset.save_dataset(args.prefix, args.suffix)
 
     # Generate a sampler to be used in the training
     sampler = torch.utils.data.distributed.DistributedSampler(
-        dataset, shuffle=True, num_replicas=node_info['world_size'], rank=rank, seed=31
+        dataset, shuffle=True, num_replicas=args.world_size, rank=args.rank, seed=args.seed
     )
 
     # Generate a loader to be used in the training
     loader = torch.utils.data.DataLoader(
-        dataset, sampler=sampler, batch_size=train_info['batch_size'], 
-        num_workers=node_info['workers'], pin_memory=True, drop_last=True
+        dataset, sampler=sampler, batch_size=args.batch_size,
+        num_workers=args.workers, pin_memory=True, drop_last=True
     )
     
     # Generate several validation sets
-    valid_sets = generate_validation_sets(dataset, train_info, node_info, rank)
+    valid_sets = generate_validation_sets(dataset, args)
 
     # Generate the model to be used in the training
-    network = spectrec_input.get_network().cuda(gpu)
+    network = spectrec_input.get_network().cuda(args.gpu)
 
     # Synchronise the batch normalisation
     network = torch.nn.SyncBatchNorm.convert_sync_batchnorm(network)
 
     # Wrap the model around a distributed dataparallel
-    network = torch.nn.parallel.DistributedDataParallel(network, device_ids=[gpu])
+    network = torch.nn.parallel.DistributedDataParallel(network, device_ids=[args.gpu])
 
-    # Generate the loss function. 
-    loss = MSELoss()
+    # Generate the loss function.
+    loss = MSELoss().cuda(args.gpu)
 
-    # Generate the optimiser to be used in the training. 
+    # Generate the optimiser to be used in the training.
     optim = torch.optim.Adam(network.parameters())
 
     # Generate a training object to train the network
-    Trainer(train_info, loader, network, loss, optim, (rank == 0)).train_and_validate(valid_sets)
+    Trainer(args, loader, network, loss, optim).train_and_validate(valid_sets)
 
 if __name__ == '__main__':
 
     # Decription message of the program
-    desc_msg = 'Train script to train a network for spectral reconstruction. The training can be distributed' + \
-               'among different nodes and GPUs to allow more efficiency.'
+    desc_msg = 'Training script for spectral reconstruction. It allows distributed data parallel jobs'
 
     # Use a command line argument parser
     parser = argparse.ArgumentParser(description=desc_msg)
 
-    # Add some arguments to the parser
+    # Add some common arguments to the parser
     parser.add_argument('-config', type=str, default='./template.yml', help='Path to configuration yaml file')
-    parser.add_argument('-gpus', type=str, default='0', help='Comma separated gpu device indices.')
-    parser.add_argument('-nodes', type=int, default=1, help='Number of nodes used in the distributed architecture.')
-    parser.add_argument('-node_rank', type=int, default=0, help='Rank corresponding to the current node.')
-    parser.add_argument('-workers', type=int, default=0, help='Number of subprocesses used when loading data.')
+    parser.add_argument('-seed', type=int, default=31, help='Seed to be used in the random engines')
+    parser.add_argument('-backend', type=str, default='nccl', help='Used torch distributed backend.')
+    
+    # Add some local run information
+    parser.add_argument('-gpus', type=str, default='0', help='Comma separated gpu device indices. Only works if not on SLURM')
+    parser.add_argument('-workers', type=int, default=2, help='Number of subprocesses used when loading data.')
+
+    # Add the slurm information to the parser
+    parser.add_argument('-slurm', action='store_true', help='Submit the scripts to SLURM')
+    parser.add_argument('-slurm-ngpus', type=int, help='Number of GPUs per node in SLURM')
+    parser.add_argument('-slurm-nnodes', type=int, help='Number of nodes used in SLURM')
+    parser.add_argument('-slurm_partition', type=str, help='Partition where the SLURM job will be submitted')
+    parser.add_argument('-slurm_shared_dir', type=str, help='Directory where SLURM output will be output')
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Initialise the distributed nodes
-    node_info = initialise_dist_nodes(args.gpus, args.nodes, args.node_rank, args.workers)
+    # Add some information to the arguments
+    args.port = random.randint(49152, 65535)
 
-    # Spawn some distributed training.
-    torch.multiprocessing.spawn(train, args=(args.config, node_info), nprocs=node_info['gpus_per_node'])
+    # If we are running on the cloud, then use the SLURM movement
+    if args.slurm:
+
+        # Generate a SLURM class to wrap the execution of 
+        class SLURMTrainer:
+            def __init__(self, args):
+                self.args = args
+
+            def __call__(self):
+                # TODO: Change this function to work with SLURM
+                initialise_dist_nodes(self.args)
+                train(None, self.args)
+
+        # Generate the slurm output folder and attach the job identifier
+        args.slurm_shared_dir = get_slurm_output_directory(args.slurm_shared_dir) / "%j"
+
+        # Generate the folder for this specific job
+        Path(args.slurm_shared_dir).mkdir(parents=True, exist_ok=True)
+
+        # Generate a submitit executor
+        executor = submitit.AutoExecutor(folder=args.slurm_shared_dir, slurm_max_num_timeout=30)
+
+        # Update some parameters on submitit
+        executor.update_parameters(
+            mem_gb=12 * args.slurm_ngpus,
+            gpus_per_node=args.slurm_ngpus,
+            tasks_per_node=args.slurm_ngpus,
+            cpus_per_task=2,
+            nodes=args.slurm_nnodes,
+            timeout_min=2800,
+            slurm_partition=args.slurm_partition,
+            name="Spectrec",
+        )
+
+        # Execute the job
+        job = executor.submit(SLURMTrainer(args))
+        print(f'Submitted job_id: {job.job_id}')
+
+    # If not, use the normal distributed dataparallel
+    else:
+
+        # Initialise the distributed nodes
+        initialise_dist_nodes(args)
+
+        # Spawn some distributed training.
+        torch.multiprocessing.spawn(train, args=(args,), nprocs=args.ngpus_per_node)
